b6216edf824d30bdfa40c1417c7b4c8e2555ffbf
lucasCicero1 <lucas.cicerofs@outlook.com>
2023-04-18 22:08:52 -0300

    feat: ensure LogControllerDecorator returns the same result of controller

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -6,7 +6,7 @@ export const adaptRoute = (controller: Controller) => {
     const httpRequest: HttpRequest = {
       body: req.body
     }
-    const httpResponse = await controller.handle(httpRequest)
-    res.status(httpResponse.statusCode).json(httpResponse.body)
+    const { statusCode, body } = await controller.handle(httpRequest)
+    res.status(statusCode).json(body)
   }
 }
