33a9d061272a866bcde8c92f423aa7647c5306ea
lucasCicero1 <lucas.cicerofs@outlook.com>
2023-04-25 20:20:31 -0300

    refactor: improve makeSut method with its interface

--- src/presentation/controllers/signup/login/login.spec.ts
+++ src/presentation/controllers/signup/login/login.spec.ts
@@ -2,13 +2,20 @@ import { LoginController } from './login'
 import { badRequest } from '../../../helpers/http-helper'
 import { MissingParamsError } from '../../../erros'
 
-const makeSut = (): LoginController => {
-  return new LoginController()
+interface SutTypes {
+  sut: LoginController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new LoginController()
+  return {
+    sut
+  }
 }
 
 describe('Login Controller', () => {
   test('Should return 400 if no email is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         password: 'any_password'
@@ -20,7 +27,7 @@ describe('Login Controller', () => {
   })
 
   test('Should return 400 if no password is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com'
