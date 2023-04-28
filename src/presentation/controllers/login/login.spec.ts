941331f1891febd0bdded8df2577f1e9a06c980a
lucasCicero1 <lucas.cicerofs@outlook.com>
2023-04-28 18:01:41 -0300

    refactor: move http helper to a folder

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,5 +1,5 @@
 import { LoginController } from './login'
-import { badRequest, serverError, unauthorized, ok } from '../../helpers/http-helper'
+import { badRequest, serverError, unauthorized, ok } from '../../helpers/http/http-helper'
 import { MissingParamError } from '../../erros'
 import type { HttpRequest, Authentication, Validation } from './login-protocols'
 
--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -7,7 +7,7 @@ import type {
   HttpRequest,
   Validation
 } from './signup-protocols'
-import { ok, serverError, badRequest } from '../../helpers/http-helper'
+import { ok, serverError, badRequest } from '../../helpers/http/http-helper'
 
 const makeAddAccount = (): AddAccount => {
   class AddAccountStub implements AddAccount {
