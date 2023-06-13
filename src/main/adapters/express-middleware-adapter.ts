fef46822dfff829fc666c1e8ab95a336fadcce49
lucasCicero1 <lucas.cicerofs@outlook.com>
2023-06-13 20:17:12 -0300

    feat: ensure add survey route returns 403 if no access token is provided

---
+++ src/main/adapters/express-middleware-adapter.ts
@@ -0,0 +1,19 @@
+import type { HttpRequest, Middleware } from '../../presentation/protocols'
+import type { NextFunction, Request, Response } from 'express'
+
+export const adaptMiddleware = (middleware: Middleware) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const httpRequest: HttpRequest = {
+      headers: req.headers
+    }
+    const { statusCode, body } = await middleware.handle(httpRequest)
+    if (statusCode === 200) {
+      Object.assign(req, body)
+      next()
+    } else {
+      res.status(statusCode).json({
+        error: body.message
+      })
+    }
+  }
+}
